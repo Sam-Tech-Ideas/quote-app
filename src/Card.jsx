e3475a0ed6800131af4a2247e22bc764a164092b
Sam-Tech-ideas <agyemangsamuel235@gmail.com>
2022-08-01 23:49:42 +0000

    card update

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,7 @@ const Card = () => {
   return (
     <div className='flex justify-center transform translate-y-48 '>
 <div class="block hover:animate-spin transform scale-90 duration-500  p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-2xl ">
-   <p></p>
+   
     <p class="font-normal text-gray-700 dark:text-gray-400"><span className='text-3xl font-bold'>"</span>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chrp<span className='text-3xl font-bold'>"</span></p>
     <h3 className='font-bold '>By: Oded Phantom</h3>
      </div>
