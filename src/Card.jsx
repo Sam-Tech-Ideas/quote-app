fe441cec83a6500967ca17a9ad41892a65db724d
Sam-Tech-ideas <agyemangsamuel235@gmail.com>
2022-08-02 23:55:53 +0000

    update

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,7 @@ const Card = () => {
 <div class="block hover:animate-spin transform scale-90 duration-500  p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-2xl ">
    
     <p class="font-normal text-gray-700 dark:text-gray-400"><span className='text-3xl font-bold'>"</span>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chrp
-    Geniuses are born not made<span className='text-3xl font-bold'>"</span></p>
+    Geniuses are born not made by albert enstiiig sdjjs sjdddddd<span className='text-3xl font-bold'>"</span></p>
     
     <h3 className='font-bold '>By: Oded Phantom</h3>
      </div>
