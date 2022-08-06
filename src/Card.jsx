5e8135ddcf7a8a7eea134baae9e6f6b496eda6ea
Sam-Tech-ideas <agyemangsamuel235@gmail.com>
2022-08-06 23:44:47 +0000

    update

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,14 +4,12 @@ const Card = () => {
   return (
     <div>
 <div className="">
-  <div className="property1Default2">
-<p className="text1">
-Hor you!</p>
+
 <div className="userDetails1">
 
 
 <div className="userDetailsText1">
-<p className="userName1">Philip Wee  hjubhusdbgfiu dsiubgfsidfigj siheb feriubger</p>
+<p className="userName1">Philip Wee  hj siheb feriubger</p>
 <p className="userJob1">Co-Founder, FireJet</p>
 </div>
 </div>
