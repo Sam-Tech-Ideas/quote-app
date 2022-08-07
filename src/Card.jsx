852ca83da9517b6da51fa5802b75dc2aa42d20ab
Sam-Tech-ideas <agyemangsamuel235@gmail.com>
2022-08-07 17:23:19 +0000

    testing tweet

--- src/Card.jsx
+++ src/Card.jsx
@@ -20,13 +20,21 @@ const Card = () => {
   return (
     <div className='min-height-[100vh] flex justify-center mx-auto m-5 text-white '>
 
-        <div className="p-3 bg-indigo-500  rounded-xl  min-height-[100px] max-w-[450px] min-h-[150px] m-20 shadow-lg shadow-slate-500">
+        <div className="p-3 bg-indigo-500  rounded-xl  min-height-[100px] max-w-[450px] min-h-[150px] m-20 mt-[250px] shadow-lg shadow-slate-500">
 
           <p className='p-3'>{quotes.text}</p>
 
-            <p className="p-2">{quotes.author}</p>
+            <p className="p-2" >Author: {quotes.author}</p>
 
-               <button className=  ' hover:bg-blue-700  hover:text-blue-300 bg-blue-500 rounded-xl ml-2 py-1 px-3'>Share</button>
+               <button onClick={getQuote} className=' hover:bg-blue-700  hover:text-blue-300 bg-blue-500 rounded-xl ml-2 py-1 px-3'>Share
+                </button>
+               <a href={`https://twitter.com/intent/tweet?text=${quotes.text}`} 
+               target="_blank" rel="noopener noreferrer">
+                <button className="bg-teal-500 py-1 px-3 rounded-lg m-2 hover:bg-green-700">
+                Tweet
+              </button>
+               </a>
+              
          </div>
      </div>
   )
