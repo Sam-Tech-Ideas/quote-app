03e2ab8f059586486cd6a0d41ca80dd1c954e839
Sam-Tech-ideas <agyemangsamuel235@gmail.com>
2022-08-07 09:01:27 +0000

    update card

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 const Card = () => {
+  const [quotes, setQuotes] = useState("");
+
+    const getQuote = () => {
+        fetch("https://type.fit/api/quotes")
+      .then(res => res.json())
+      .then(data => {
+      let randomNum = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomNum]);
+    })
+  };
+
+  useEffect(() => {
+        getQuote()
+  }, [])
+
   return (
-    <div>
-<div className="">
-
-<div className="userDetails1">
-
-
-<div className="userDetailsText1
-</div>
-</div>
-</div>
-</div>
-
+    <div className='App flex justify-center mx-auto m-5 text-white'>
+      <div className="bg-green-500 m-5 rounded-xl w-full md-min-height ">
+      <p className='p-3'>{quotes.text}</p>
+     <p className="p-2">{quotes.author}</p>
+     <button className='btn bg-blue-500 flex justify-end'>Share</button>
       </div>
+    
+    </div>
   )
 }
 
 export default Card
-
